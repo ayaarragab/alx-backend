ffa98f9e52a2afe7f1edae513cc6a78d4c1b8786
ayaarragab <ayarsk4343@gmail.com>
2024-07-21 18:59:10 +0300

    0. Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,7 +7,6 @@ page and page_size
 from typing import Tuple
 
 
-
 def index_range(page: int, page_size: int) -> Tuple:
     """
     index_range
@@ -17,4 +16,4 @@ def index_range(page: int, page_size: int) -> Tuple:
     if page == 1:
         return (0, page_size)
     else:
-        return ((page - 1) * page_size , page * page_size)
+        return ((page - 1) * page_size, page * page_size)
