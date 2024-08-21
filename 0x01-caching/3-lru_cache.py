27f88554ab33c2116d3ec3e56610826daa2ae4ac
ayaarragab <ayarsk4343@gmail.com>
2024-08-21 20:07:15 +0300

    Task lru cache done

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,47 +1,39 @@
 #!/usr/bin/env python3
+"""Class representation of LRU caching
 """
-LRUCache cache?
-"""
-
-
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
+    """"LRUCache that inherits from BaseCaching
     """
-    LRU cache class
-    """
-
     def __init__(self):
-        """
-        d
+        """Initialize LRUCache
         """
         super().__init__()
         self.queue = []
 
     def put(self, key, item):
+        """Assign key and item to the cache system
         """
-        to update
-        """
-        if not key or not item:
-            return
-        if len(self.cache_data) == self.MAX_ITEMS and not key in self.queue:
-            d_key = self.queue[0]
-            self.queue.pop(0)
-            del self.cache_data[d_key]
-            print(f'DISCARD {d_key}')
+        if len(self.cache_data) == self.MAX_ITEMS and key not in self.queue:
+            discard = self.queue.pop(0)  # Dequeue key
+            del self.cache_data[discard]
+            print("DISCARD: {}".format(discard))
+
         if key and item:
             if key in self.cache_data:
-                self.queue.remove(key)
-            self.queue.append(key)
+                self.queue.remove(key)  # remove from the queue
+            self.queue.append(key)  # enqueue key
             self.cache_data[key] = item
 
     def get(self, key):
+        """Fetch data from the cache system with key
         """
-        retrieve data
-        """
-        if not key or key not in self.cache_data.keys():
+        if not key or key not in self.cache_data:
             return None
+        # Remove from any position in the queue and add to the back
         self.queue.remove(key)
         self.queue.append(key)
+
         return self.cache_data[key]
