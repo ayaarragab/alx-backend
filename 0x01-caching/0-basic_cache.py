ab52a5b23ecb2b4a8b7fa0eccde9c701cb77009a
ayaarragab <ayarsk4343@gmail.com>
2024-08-21 16:46:39 +0300

    task 0 done

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+"""
+basic cache?
+"""
+
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    basic cache
+    """
+
+    def put(self, key, item):
+        """
+        to update
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        retrieve data
+        """
+        if not key or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
