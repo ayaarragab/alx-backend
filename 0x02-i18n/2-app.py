43f73e08a85976e8e9d85c3122f2bb17a5729ed1
ayaarragab <ayarsk4343@gmail.com>
2024-08-28 02:51:28 +0300

    3rd

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""a basic Flask app
+"""
+from flask import Flask, render_template, g, request
+from flask_babel import Babel, localeselector
+
+
+class Config:
+    """
+    config class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def hello() -> str:
+    """Create a single / route and an
+    index.html template that simply
+    outputs “Welcome to Holberton”
+    as page title (<title>) and
+    “Hello world” as header (<h1>).
+    """
+    return render_template('0-index.html')
+
+
+@localeselector
+def get_locale():
+    """to determine the best
+    match with our supported languages.
+    """
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run()
