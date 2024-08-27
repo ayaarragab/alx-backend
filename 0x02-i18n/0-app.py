716190138f0ecddbc45ac10cc0cee89495ef8ca3
ayaarragab <ayarsk4343@gmail.com>
2024-08-28 02:29:51 +0300

    1st

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""a basic Flask app
+"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Create a single / route and an
+    index.html template that simply
+    outputs “Welcome to Holberton”
+    as page title (<title>) and
+    “Hello world” as header (<h1>).
+    """
+    return render_template('templates/0-index.html')
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
