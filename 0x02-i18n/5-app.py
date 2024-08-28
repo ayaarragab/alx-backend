d88af15a07c1883b8e674aa71ee3ba0aa4bf44e2
ayaarragab <ayarsk4343@gmail.com>
2024-08-28 03:46:24 +0300

    2-app

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""a basic Flask app
+"""
+from flask import Flask, render_template, g, request
+from flask_babel import Babel, gettext as _
+
+
+class Config:
+    """
+    config class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+@babel.localeselector
+def get_locale():
+    """_summary_
+
+    Returns:
+            _type_: _description_
+    """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        print(locale)
+        return locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def hello() -> str:
+    """Create a single / route and an
+    index.html template that simply
+    outputs “Welcome to Holberton”
+    as page title (<title>) and
+    “Hello world” as header (<h1>).
+    """
+    return render_template(
+        '2-index.html',
+        title=_("home_title"),
+        header=_("home_header"))
+
+def get_user():
+    """returns a user dictionary or None if the ID cannot be found
+    """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request():
+    g.user = get_user()
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run()
